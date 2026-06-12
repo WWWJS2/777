a5fb04e25852b6d37616ad1a9aa841b16b494367
WWWJS2 <119140382+WWWJS2@users.noreply.github.com>
2026-06-12 05:23:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
-import WhoWeAre from "./pages/WhoWeAre";
-import OurServices from "./pages/OurServices";
-import Branches from "./pages/Branches";
-import Franchise from "./pages/Franchise";
-import ContactUs from "./pages/ContactUs";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
+import FarmLanding from "./pages/FarmLanding";
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<WhoWeAre />} />
-        <Route path="/services" element={<OurServices />} />
-        <Route path="/branches" element={<Branches />} />
-        <Route path="/franchise" element={<Franchise />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
-    </>
-  );
+  return <FarmLanding />;
 }
